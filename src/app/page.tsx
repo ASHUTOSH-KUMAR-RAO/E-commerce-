2f6bc3001e094c3078dc4d1f4ab8613b6a424341
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-06-03 23:29:59 +0530

    Changed Font Family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 const page = () => {
   return (
-  <div className="min-h-screen flex items-center justify-center">
-    <p className="text-red-600 underline text-2xl">Hello World</p>
+    <div className="p-10">
+  <div className="flex flex-col gap-y-4">
+    <div>
+      <Button>
+        kaise ho Baby
+      </Button>
+    </div>
+
+    <div>
+      <Input placeholder="kaise ho input Bhaiya"/>
+    </div>
+
+    <div>
+      <Progress value={30}/>
+    </div>
+
+    <div>
+      <Textarea placeholder="Mai Text Area Hu"/>
+    </div>
+  </div>
   </div>
   )
 }
